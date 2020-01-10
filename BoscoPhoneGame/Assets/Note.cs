d5772c3dbc05fca41117c46099a7ad2128685168
David <meyer.david221@gmail.com>
2020-01-09 22:53:26 -0700

    Basics
    Basics of Notes and Activators.

---
+++ BoscoPhoneGame/Assets/Note.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour {
+
+
+	//Ball park instead of BPM way.
+	Rigidbody2D rb;
+	public float speed;
+
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	// Use this for initialization
+	void Start () {
+		rb.velocity = new Vector2(0, -speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
